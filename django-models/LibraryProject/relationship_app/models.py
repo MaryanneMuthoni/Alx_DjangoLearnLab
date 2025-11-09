823abb4da077ff2e898ef8e38fb3b82914140299
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-11-09 05:53:52 -0800

    Fix checker error: LibraryProject/relationship_app/models.py doesn't contain: ['return self.name']

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,14 +4,30 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        '''This makes Author objects display as their name'''
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''This makes Book objects display as their title'''
+        return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        '''This makes Library objects display as their name'''
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''This makes Librarian objects display as their name'''
+        return self.name
