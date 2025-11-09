cc94d29f5f3a74715151e01d8b1ec9e1eebaff77
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-11-09 08:25:52 -0800

    In your existing Django project, enhance the relationship_app by adding new views that display information about books and libraries. Implement both function-based and class-based views to handle these displays and configure the URL patterns to route these views correctly.
    Steps:
    Implement Function-based View:
    
    Create a function-based view in relationship_app/views.py that lists all books stored in the database.
    This view should render a simple text list of book titles and their authors.
    Implement Class-based View:
    
    Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
    Utilize Django’s ListView or DetailView to structure this class-based view.
    Configure URL Patterns:
    
    Edit relationship_app/urls.py to include URL patterns that route to the newly created views. Make sure to link both the function-based and class-based views.
    Create Templates (Optional for Display):
    
    For a more structured output, using the code below as templates for each view to render the information in HTML format instead of plain text.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+
+def book_list(request):
+    '''Lists all books stored in the database
+    view should render a simple text list of book titles and their authors'''
+    books = Book.objects.select_related('author').all()
+    context = {'books': books}
+    return render(request, 'books/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    '''displays details for a specific library, listing all books available in that library'''
+    model = Library
+    template_name = 'libraries/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        """Add all books available in this library to the context."""
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
