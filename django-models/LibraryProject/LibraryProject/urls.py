e456800be0ebb5e50f91c6e6eb3069badcb21c9b
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-11-09 09:55:22 -0800

    Fix error- LibraryProject/relationship_app/views.py doesn't contain: ['from django.contrib.auth import login']

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from relationship_app import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.book_list, name='books'),
     path('libraries/', views.LibraryDetailView.as_view(), name='libraries'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
