1fbfc2cafe5be83d11434abe6d94164c2dd2b041
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-11-16 11:44:35 -0800

    Fix error

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
-
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Book Title'}),
+            'author': forms.TextInput(attrs={'placeholder': 'Author Name'}),
+            'publication_year': forms.NumberInput(attrs={'placeholder': 'Publication Year'}),
+        }
