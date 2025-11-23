36b9a8c90a71a2917dfb13b360583b7bcfe7af2a
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-11-23 02:53:12 -0800

    0. Setting Up a New Django Project with Django REST Framework
    In this task, you will create a new Django project, configure Django REST Framework, and prepare the environment for future tasks focused on building APIs.

---
+++ api_project/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    '''Class representing student instance'''
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    GENDER = [
+            ('M', 'Male'), 
+            ('F', 'Female'),
+            ('O', 'Other'),
+            ]
+
+    gender = models.CharField(max_length=1, choices=GENDER, blank=False, null=False)
+    date_of_birth = models.DateField()
+    age = models.IntegerField()
+    grade = models.IntegerField()
+    teacher = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['grade', 'first_name']
