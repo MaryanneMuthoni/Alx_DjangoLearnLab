89123756c181607990e52abf3abc0ccb641fa6ef
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-11 08:45:55 -0800

    Fix: LibraryProject/relationship_app/query_samples.py doesn't contain: ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,16 +3,16 @@ from .models import Author, Book, Library, Librarian
 # Contain the query for each of the following of relationships:
 
 # Query all books by a specific author
-Book.objects.all()
-author.all()
+Author.objects.get(name=author_name)
+objects.filter(author=author)
 
 # List all books in a library.
 Library.objects.get(name=library_name)
 books.all()
 
 # Retrieve the librarian for a library
-Librarian.objects.get(name=librarian_name)
-library.all()
+Library.objects.get(name=library_name)
+objects.filter(library=library)
 
 if __name__ == "__main__":
     # code to run when the script is executed directly
