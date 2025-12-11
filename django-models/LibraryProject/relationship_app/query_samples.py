94f0b5ff990c0049e0459d78961708c553265549
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-11 07:16:24 -0800

    Fix: did an f string on return instead of just returning the variable in __str__ method

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,7 @@
-# contain the query for each of the following of relationships
-
 from .models import Author, Book, Library, Librarian
+
+# Contain the query for each of the following of relationships:
+
 # Query all books by a specific author
 book.author.name
 
@@ -9,3 +10,8 @@ books.library.all()
 
 # Retrieve the librarian for a library
 librarian.library.name
+
+if __name__ == "__main__":
+    # code to run when the script is executed directly
+    main()
+
