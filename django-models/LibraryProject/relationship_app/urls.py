c17bee16b3cacd3f9e7e073905c89a0fa0cdd68b
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-12 08:17:54 -0800

    Fix: LibraryProject/relationship_app/urls.py doesn't contain: ["from .views import list_books"]

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
-    path('list/', views.list_view, name='list'),
+    path('list/', views.list_books, name='list'),
     path('library/', views.LibraryDetailView.as_view(), name='library'),
 ]
