eb992d9bc8f472f179f8ab5d3b79fedf0b718ed5
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-12 12:24:53 -0800

    Fix: LibraryProject/relationship_app/urls.py doesn't contain: ["views.register"]

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 from .views import LibraryDetailView
 from .views import list_books
-from .views import SignUpView
+from .views import register
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('signup/', SignUpView.as_view(template_name='relationship_app/register.html'), name='register'),
+    path('signup/', views.register.as_view(template_name='relationship_app/register.html'), name='register'),
 ]
