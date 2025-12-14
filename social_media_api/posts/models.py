76f902e4a5ba38b305ae4e6a1e29797c8207f0b6
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-14 12:26:16 -0800

    1. Implementing Posts and Comments Functionality

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class Comment(models.Model):
+    post = models.ManyToManyField(Post)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/accounts/', include('accounts.urls'))
+    path('api/', include('posts.urls')),
+    path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
