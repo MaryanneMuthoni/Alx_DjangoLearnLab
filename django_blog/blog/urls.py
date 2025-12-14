8f279f71df9584d72ef35c9f63978e47f720c17a
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-12-14 07:06:52 -0800

    Fix: blog/views.py doesn't contain: ["POST", "method", "save()"]

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', SignUpView.as_view(), name='signup'),
-    path('profile/', profile_view, name='profile'),
-    path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('profile/', profile, name='profile'),
+    path('profile/update/', profile_view, name='profile_update'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView, UpdateView
 from .forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -12,14 +12,21 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'blog/signup.html'
 
-def profile_view(request):
+def profile(request):
     return render(request, 'profile.html')
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
-    model = User
-    fields = ['username', 'email']
-    template_name = 'blog/profile_update.html'
-    success_url = 'profile'
+@login_required
+def profile_view(request):
+    """
+    Allows authenticated users to view and edit their profile details.
+    Handles POST requests to update user information.
+    """
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
 
-    def get_object(self):
-        return self.request.user
+    return render(request, 'blog/profile.html', {'form': form})
